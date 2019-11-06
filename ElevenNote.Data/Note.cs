550469803814406ab0b07f81e36d2b00b693cb6a
Max Edwards <medwards2099@gmail.com>
2019-11-06 10:04:47 -0500

    api first push

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public class Note
+    {
+        [Key] //Primary Key of the corresponding database
+        public int NoteId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+
+        [DefaultValue(false)]
+        public bool IsStarred { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
---
+++ ElevenNote.WebMVC/Controllers/WebApi/NoteController.cs
@@ -0,0 +1,44 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using AuthorizeAttribute = System.Web.Http.AuthorizeAttribute;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+using RouteAttribute = System.Web.Http.RouteAttribute;
+using RoutePrefixAttribute = System.Web.Http.RoutePrefixAttribute;
+
+namespace ElevenNote.WebMVC.Controllers.WebApi
+{
+    [Authorize]
+    [RoutePrefix("api/Note")]
+    public class NoteController : ApiController
+    {
+        private bool SetStarState( int noteId, bool newState)
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+
+            var service = new NoteService(userId);
+
+            var detail = service.GetNoteById(noteId);
+
+            var updatedNote =
+                new NoteEdit
+                {
+                    NoteId = detail.NoteId,
+                    Title = detail.Title,
+                    Content = detail.Content,
+                    IsStarred = newState
+                };
+            return service.UpdateNote(updatedNote);
+        }
+
+        [Route("{id}/Star")]
+        [HttpPost]
+        public bool ToggleStarOn(int id) => SetStarState(id, true);
+    }
+}
